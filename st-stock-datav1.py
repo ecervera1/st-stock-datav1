073930bfb18668645f418f993a03fec74432eb66
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-01-29 21:51:10 -0600

    Update st-stock-datav1.py

--- st-stock-datav1.py
+++ st-stock-datav1.py
@@ -143,7 +143,7 @@ if st.button('Run'):
     #fig, axs = plt.subplots(len(tickers), 6, figsize=(24, 8 * len(tickers)))
 
     num_subplots = len(tickers)
-    figsize_width = 8  # Width of each subplot
+    figsize_width =  st.pyplot(fig, use_container_width=True)/6 # Width of each subplot
     figsize_height = num_subplots * 4  # Height of the entire figure
 
     # Create a figure with subplots: X columns (Ticker, Market Cap, Revenue, Financial Metrics...) for each ticker
