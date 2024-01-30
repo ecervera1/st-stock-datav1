fbbf4baa67c3978606b6365e1fd2393f40b872c9
ecervera1 <157933641+ecervera1@users.noreply.github.com>
2024-01-29 21:31:06 -0600

    Update st-stock-datav1.py

--- st-stock-datav1.py
+++ st-stock-datav1.py
@@ -163,6 +163,6 @@ if st.button('Run'):
     #plt.subplots_adjust(wspace=0.5)
     plt.tight_layout()
     st.pyplot(fig, use_container_width=True)
-    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    #plt.subplots_adjust(wspace=0.5, hspace=0.5)
     st.pyplot(fig)
 
